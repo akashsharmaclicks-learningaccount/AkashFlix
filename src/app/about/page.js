023ef26b7aeb4a7d64796d2d5f0b2da31b396901
Akash Sharma <akash.sharma@igniteconnex.com>
2024-04-29 19:20:22 +0530

    Made dynamic paragraph for about page.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,15 @@
 import Herosection from "../components/Herosection";
+
 const About = () => {
-  return <Herosection title={"OUR STORY"} imageUrl={"/about1.svg"} />;
+  const paragraph =
+    "Embark on a cinematic adventure with us! Our collection spans from captivating award-winning dramas to pulse-pounding blockbuster action films. Dive into our diverse selection of the latest and greatest movies, and uncover your next obsession today.";
+  return (
+    <Herosection
+      title={"OUR STORY"}
+      paragraph={paragraph}
+      imageUrl={"/about1.svg"}
+    />
+  );
 };
+
 export default About;
