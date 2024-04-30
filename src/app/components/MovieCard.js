3fcf9b299ba8871e3638371f81bce12c9fdea78c
Akash Sharma <akash.sharma@igniteconnex.com>
2024-04-30 13:16:15 +0530

    Added new more properties from response data object.

--- src/app/components/MovieCard.js
+++ src/app/components/MovieCard.js
@@ -2,7 +2,7 @@ import styles from "@/app/styles/common.module.css";
 import Image from "next/image";
 import Link from "next/link";
 const MovieCard = (curlElem) => {
-  const { id, type, title, synopsis } = curlElem.jawSummary;
+  const { id, type, title, synopsis, genres, releaseYear } = curlElem.jawSummary;
   return (
     <>
       <div className={styles.card}>
@@ -16,7 +16,11 @@ const MovieCard = (curlElem) => {
         </div>
         <div className={styles.card_data}>
           <h2>{title.substring(0, 18)}</h2>
+          <h3>TYPE : {type}</h3>
+          <h3>GENRE : {genres[0].name}</h3>
+          <h3>RELEASE YEAR : {releaseYear}</h3>
           <p>{`${synopsis.substring(0, 66)} ...`}</p>
+
           <Link href={`/movie/${id}`}>
             <button>Read More</button>
           </Link>
