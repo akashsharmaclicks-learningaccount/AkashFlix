edf5afa84ea65b24309b6c9e30dcc3086663f0dc
Akash Sharma <akash.sharma@igniteconnex.com>
2024-04-30 13:16:52 +0530

    Removed unnecessary logs.

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -7,7 +7,6 @@ import { useState } from "react";
 
 const Nav = () => {
   const [openMenu, setOpenMenu] = useState(false);
-  console.log("value " + openMenu);
   return (
     <>
       <nav className={styles.navbar}>
